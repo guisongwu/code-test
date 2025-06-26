e700fb81023119a2139f9aac9f6cde6b6cfe23eb
guisongwu <wugsong2001@163.com>
2025-06-26 20:52:37 +0800

    20250626 last commit

--- testc.c
+++ testc.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 
 int main(int argc, char* argv[]) {
+    // ----------------------------------- char ---------------------------------------
+    char a = 65;
+    printf("%c\n", a);
+    // ------------------------- COORD *verts and COORD *verts[2] -----------------------------
+    /* typedef double COORD[3]; */
+    /* COORD *vert; */
+    /* vert = (COORD *) calloc (2, sizeof(COORD)); */
+    /* for (int i = 0; i < 2; i++) { */
+    /*     for (int j = 0; j < 3; j++) { */
+    /*         vert[i][j] = (double)i*j; */
+    /*         printf("%lf\t", vert[i][j]); */
+    /*     } */
+    /*     printf("\n"); */
+    /* } */
+
+    /* COORD vert1 = {1.0, 2.0, 3.0}; */
+    /* COORD vert2 = {4.0, 5.0, 6.0}; */
+    /* COORD *verts[2] = {&vert1, &vert2}; */
+    /* for (int i = 0; i < 2; i++) { */
+    /*     for (int j = 0; j < 3; j++) { */
+    /*         printf("%lf\t", (*verts[i])[j]); */
+    /*     } */
+    /*     printf("\n"); */
+    /* } */
     // ------------------------------------ stdout(1) and stderr(2) -------------------------------------
     /* printf("This goes to stdout.\n"); */
     /* fprintf(stdout, "This is stdout using fprintf.\n"); */
@@ -12,7 +37,7 @@ int main(int argc, char* argv[]) {
     // ./testc > all.txt 2>&1
     // ./testc &> all.txt
     // ------------------------------------ M_PI --------------------------------------------
-    printf("%lf\n", M_PI);
+    /* printf("%lf\n", M_PI); */
 
     return 0;
 }
